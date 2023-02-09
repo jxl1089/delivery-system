4f2b4046dda5b3cc1ca7dd24fd392a42c8e3c01d
Jin Lee <111788751+jxl1089@users.noreply.github.com>
2023-02-09 15:02:26 +0900

    Delete testlol directory

--- testlol/src/module-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * 
- */
-/**
- * @author tmdwn
- *
- */
-module testlol {
-}
--- testlol/src/testlol/testlol.java
+++
@@ -1,7 +0,0 @@
-package testlol;
-
-public class testlol {
-	public static void main(String[] args) {
-		System.out.println("이소민");
-	}
-}
