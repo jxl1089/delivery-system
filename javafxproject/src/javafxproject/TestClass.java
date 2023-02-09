d82d0d300e9cade454cad70837965be1555680f3
Flex <Flex@DESKTOP-2563T4R>
2023-02-09 10:50:58 +0900

    ㅇㅇㅇㅇ

--- javafxproject/src/javafxproject/TestClass.java
+++ javafxproject/src/javafxproject/TestClass.java
@@ -5,11 +5,6 @@ public class TestClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("이2런");
-<<<<<<< HEAD
-		System.out.println("이3런");
-=======
-		System.out.println("고치고올리기");
->>>>>>> branch 'master' of https://github.com/jxl1089/delivery-system
+		System.out.println("고치고 올리기");
 	}
-
 }
