0294e723faa0412bea26204ea9188db1434a4654
lyn <lyn@192.168.0.33>
2023-02-09 11:35:09 +0900

    이승

--- javafxproject/src/javafxproject/TestClass.java
+++ javafxproject/src/javafxproject/TestClass.java
@@ -6,5 +6,6 @@ public class TestClass {
 		// TODO Auto-generated method stub
 		System.out.println("이2런");
 		System.out.println("고치고 올리기");
+		System.out.println("이승");
 	}
 }
---
+++ javafxproject/src/javafxproject/test.java
@@ -0,0 +1,5 @@
+package javafxproject;
+
+public class test {
+
+}
