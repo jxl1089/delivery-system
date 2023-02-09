f0e653de83420827ca373f5560f4a9c36a352e23
pheonixpark <90171172+pheonixpark@users.noreply.github.com>
2023-02-09 17:20:44 +0900

    멤버 인터페이스

---
+++ javafxproject/memberservice/CommonService.java
@@ -0,0 +1,10 @@
+package memberservice;
+
+import javafx.event.ActionEvent;
+
+public interface CommonService{
+	
+	void cancleEvent(ActionEvent event);
+	void errorMsg(String subject, String head, String content);
+
+}
---
+++ javafxproject/memberservice/LoginService.java
@@ -0,0 +1,9 @@
+package memberservice;
+
+import javafx.scene.Parent;
+
+public interface LoginService {
+	void loginProc(Parent root);
+	void membershipProc(Parent root);
+	void memberInfo();
+}
