5fb083622d5ca94d617480b85a07049c61793f59
12dlw <12dlw@DESKTOP-GG4U9MR>
2023-02-09 10:41:14 +0900

    dd

---
+++ javafxproject/src/javafxproject/TestClass.java
@@ -0,0 +1,11 @@
+package javafxproject;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("이2런");
+		System.out.println("이3런");
+	}
+
+}
